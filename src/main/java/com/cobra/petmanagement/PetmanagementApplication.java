f6c6ffa8fc66ba721730bc8976893fd318c4450a
Habandeh <hashem_abandeh@yahoo.com>
2022-04-22 14:09:41 +0300

    initial commit

---
+++ src/main/java/com/cobra/petmanagement/PetmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.cobra.petmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetmanagementApplication.class, args);
+	}
+
+}
